a00bd864a3923a29d8606ef0033ec4083ae461b5
HolzmanoLagrene <lenz.baumann2>
2018-04-10 15:48:25 +0200

    test

--- derivatives_of_the_random_walk.py
+++ derivatives_of_the_random_walk.py
@@ -1,1 +1,9 @@
-import somepackage
+from twarc import Twarc
+import json
+
+cred = json.load(open("./twitter_credentials"))
+
+
+t = Twarc(cred["api_key"], cred["cons_sec"], cred["acc_token"], cred["acc_token_sec"])
+for tweet in t.search("ferguson"):
+    print(tweet)
